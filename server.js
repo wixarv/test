a9bedd8bc51c994b033c1127775b861c074d6070
aarav dubey <wixarv@aaravs-MacBook-Air.local>
2025-04-19 09:36:32 +0530

    Refactored server and app separation for Vercel

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL || "https://diwaligift.site/",
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
     methods: ["GET", "POST"],
     credentials: true,
   },
